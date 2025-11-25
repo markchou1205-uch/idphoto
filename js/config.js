1f0be7f9bd92166d2f7ffa7ba49d7f0488e4ae9b
markchou1205-uch <markchou1205@uch.edu.tw>
2025-11-25 08:51:28 +0800

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,32 @@
+export const API_BASE_URL = "https://video.pdfsolution.dpdns.org"; 
+
+export const DEFAULT_SPECS = {
+    "passport": { 
+        "name": "護照 / 身分證", 
+        "desc": "2吋 (35x45mm) - 頭部 3.2~3.6cm", 
+        "width_mm": 35, "height_mm": 45, 
+        "ratio": 35/45,
+        "face_multiplier": 1.85, "top_margin": 0.09 
+    },
+    "resume": { 
+        "name": "健保卡 / 履歷 / 半身照", 
+        "desc": "2吋 (42x47mm)", 
+        "width_mm": 42, "height_mm": 47,
+        "ratio": 42/47,
+        "face_multiplier": 2.5, "top_margin": 0.15 
+    },
+    "inch1": { 
+        "name": "駕照 / 執照 / 證書", 
+        "desc": "1吋 (28x35mm)", 
+        "width_mm": 28, "height_mm": 35,
+        "ratio": 28/35,
+        "face_multiplier": 2.0, "top_margin": 0.12 
+    },
+    "visa_us": { 
+        "name": "美國簽證", 
+        "desc": "5x5cm (51x51mm)", 
+        "width_mm": 51, "height_mm": 51,
+        "ratio": 1.0,
+        "face_multiplier": 2.2, "top_margin": 0.15 
+    }
+};
